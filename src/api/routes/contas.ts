b2808bcb45489a60ef1c9804acb06f723693d88b
Anderson Bargas <anderson@andersonbargas.com>
2020-04-20 19:32:31 -0300

    Devolução da conta e erro.

--- src/api/routes/contas.ts
+++ src/api/routes/contas.ts
@@ -14,21 +14,21 @@ export default (app: Router) => {
         const numeroDaConta = parseInt(req.params['numeroDaConta']);
         
         contasController.depositar(valorDeposito, numeroDaConta)
-        .then(() => {
-            res.status(204).send();
+        .then((conta) => {
+            res.status(200).send(conta);
         })
         .catch(err => {
             next(err);
         });
     });
     
-    route.get('/:numeroDaConta/saque/', (req, res, next) => {
+    route.post('/:numeroDaConta/saque/', (req, res, next) => {
         const valorDeposito = req.body['valor'];
         const numeroDaConta = parseInt(req.params['numeroDaConta']);
 
         contasController.sacar(valorDeposito, numeroDaConta)
-        .then(() => {
-            res.status(204).send();
+        .then((conta) => {
+            res.status(200).send(conta);
         })
         .catch(err => {
             next(err);
