b9a9004d06ddbc66abbc0be43454827afa9d536a
Anderson Bargas <anderson@andersonbargas.com>
2020-04-19 23:32:01 -0300

    Promovendo o tipo Conta para uma classe.

--- src/models/conta.ts
+++ src/models/conta.ts
@@ -1,7 +1,28 @@
 import { tipoContas } from "../enums/tipoContas";
 
-export type Conta = {
-    numero: number;
-    saldo: number;
-    tipo: tipoContas;
-};
+
+export class Conta {
+
+    private _numero: number;
+    private _saldo: number = 0;
+    private _tipo: tipoContas;
+
+
+    constructor (numero: number, tipo: tipoContas) {
+        this._numero = numero;
+        this._tipo = tipo;
+    }
+
+    get numero(): number {
+        return this._numero;
+    }
+
+    get saldo(): number {
+        return this._saldo;
+    }
+
+    get tipo(): tipoContas {
+        return this._tipo;
+    }
+
+}
