d0399a852bfbad2287ffecfbb2b7f431b17f7e91
Anderson Bargas <anderson@andersonbargas.com>
2020-04-20 00:00:10 -0300

    Rotas para saque e depósito.

--- src/api/routes/contas.ts
+++ src/api/routes/contas.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import contasController from '../../controllers/contas';
+import { Conta } from '../../models/conta';
+import contas from '../../controllers/contas';
 
 const route = Router();
 
@@ -6,12 +9,30 @@ export default (app: Router) => {
 
     app.use('/contas', route);
 
-    route.post('/:numeroDaConta/deposito', (req, res) => {
+    route.post('/:numeroDaConta/deposito', (req, res, next) => {
+        const valorDeposito = req.body['valor'];
+        const numeroDaConta = parseInt(req.params['numeroDaConta']);
         
+        contasController.depositar(valorDeposito, numeroDaConta)
+        .then(() => {
+            res.status(204).send();
+        })
+        .catch(err => {
+            next(err);
+        });
     });
     
-    route.get('/:numeroDaConta/saque/', (req, res) => {
-        
+    route.get('/:numeroDaConta/saque/', (req, res, next) => {
+        const valorDeposito = req.body['valor'];
+        const numeroDaConta = parseInt(req.params['numeroDaConta']);
+
+        contasController.sacar(valorDeposito, numeroDaConta)
+        .then(() => {
+            res.status(204).send();
+        })
+        .catch(err => {
+            next(err);
+        });
     });
 
 
