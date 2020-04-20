f5209153d1736f0837bbffe7c43a832a71dd702f
Anderson Bargas <anderson@andersonbargas.com>
2020-04-20 19:32:01 -0300

    Devolução da conta e erro.

--- src/controllers/contas.ts
+++ src/controllers/contas.ts
@@ -9,27 +9,27 @@ const banco = new Banco();
 banco.adicionarConta(new Conta(1, tipoContas.Corrente));
 banco.adicionarConta(new Conta(2, tipoContas.Corrente));
 
-async function depositarValorEmContaPorNumero (valorDeposito: number, numeroDaConta: number) : Promise<void> {
-    return new Promise<void>((res, rej) => {
+async function depositarValorEmContaPorNumero (valorDeposito: number, numeroDaConta: number) : Promise<Conta> {
+    return new Promise<Conta>((res, rej) => {
         try {
             const conta = banco.getContaPorNumero(numeroDaConta);
             conta.depositar(valorDeposito);
-            res();
+            res(conta);
         } catch (err) {
-            rej();
+            rej(err);
         }
     });
 
 };
 
-async function sacarValorDeContaPorNumero (valorSaque: number, numeroDaConta: number) : Promise<void> {
-    return new Promise<void>((res, rej) => {
+async function sacarValorDeContaPorNumero (valorSaque: number, numeroDaConta: number) : Promise<Conta> {
+    return new Promise<Conta>((res, rej) => {
         try {
             const conta = banco.getContaPorNumero(numeroDaConta);
             conta.sacar(valorSaque);
-            res();
+            res(conta);
         } catch (err) {
-            rej();
+            rej(err);
         }
     });
 }
